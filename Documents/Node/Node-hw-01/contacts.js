67566fb5d0137a36bb3bc4d410b65b8ded4c0528
SvetlanaRomaniuk111 <gatskosv@gmail.com>
2023-11-19 16:40:29 +0200

    first commit

---
+++ Documents/Node/Node-hw-01/contacts.js
@@ -0,0 +1,43 @@
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+
+const contactsPath = path.resolve("db", "contacts.json");
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const res = contacts.find(({ id }) => id === contactId);
+  return res || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts(); 
+  const res = contacts.find(({ id }) => id === contactId);
+  res
+    ? await fs.writeFile(
+      contactsPath,
+      JSON.stringify(contacts.filter(({ id }) => id !== contactId), null, 2)
+    )  
+    : null;
+  return res || null;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+module.exports = { listContacts, getContactById, removeContact, addContact }
